2587d3086417a999c1c396d1afd2668a8e6848c8
andi49 <46962871+andi49@users.noreply.github.com>
2025-11-14 09:42:30 +0100

    input for username

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+/// User enter name here after it remvoes the input field and button 
+/// display later user name and a list where highscore etc will display.
+
+document.querySelector('form').addEventListener('submit', function(event) {
+     event.preventDefault()
+
+     const userName = document.querySelector('input').value
+     document.querySelector('h1').textContent = userName
+
+     document.querySelector('input').style.display = ('none')
+     document.querySelector('button').style.display = ('none')
+     document.getElementById('myList').classList.remove('listOfNums')
+})
+
+
+
